3278da12df068aeb2a2f7b5fb6ea64896220987b
Jack Ullery <jmu17@pitt.edu>
2022-01-09 14:18:11 -0500

    run unit-tests from constructor of GTK ApplicationWindow, add tests for status.cc

--- test/src/tabs/status.cc
+++ test/src/tabs/status.cc
@@ -1,6 +1,23 @@
 #include "status_mock.cc"
 
-TEST(Status, FILTER_FFF){
+#include <chrono>
+#include <thread>
+
+class StatusTest : public ::testing::Test
+{
+public:
+	MOCK_METHOD(void, handle_signal, ());
+
+protected:
+	StatusTest(){}
+	virtual void SetUp(){}
+
+	void click_everything(Gtk::Widget *obj);
+
+	StatusMock sm;
+};
+
+TEST_F(StatusTest, FILTER_FFF){
 	bool res = StatusMock::filter("abcdEFGH", "cDEf", false, false, false);
 	EXPECT_TRUE(res);
 
@@ -8,7 +25,7 @@ TEST(Status, FILTER_FFF){
 	EXPECT_FALSE(res);
 }
 
-TEST(Status, FILTER_TFF){
+TEST_F(StatusTest, FILTER_TFF){
 	bool res = StatusMock::filter("abcdEFGH", "c*f", true, false, false);
  	EXPECT_TRUE(res);
 
@@ -16,7 +33,7 @@ TEST(Status, FILTER_TFF){
 	EXPECT_FALSE(res);
 }
 
-TEST(Status, FILTER_FTF){
+TEST_F(StatusTest, FILTER_FTF){
 	bool res = StatusMock::filter("abcdEFGH", "cdEF", false, true, false);
 	EXPECT_TRUE(res);
 
@@ -24,7 +41,7 @@ TEST(Status, FILTER_FTF){
 	EXPECT_FALSE(res);
 }
 
-TEST(Status, FILTER_FFT){
+TEST_F(StatusTest, FILTER_FFT){
 	bool res = StatusMock::filter("abcdEFGH", "abcdEFGh", false, false, true);
 	EXPECT_TRUE(res);
 
@@ -32,7 +49,7 @@ TEST(Status, FILTER_FFT){
 	EXPECT_FALSE(res);
 }
 
-TEST(Status, FILTER_TTF){
+TEST_F(StatusTest, FILTER_TTF){
 	bool res = StatusMock::filter("abcdEFGH", "c.*F", true, true, false);
 	EXPECT_TRUE(res);
 
@@ -40,7 +57,7 @@ TEST(Status, FILTER_TTF){
 	EXPECT_FALSE(res);
 }
 
-TEST(Status, FILTER_TFT){
+TEST_F(StatusTest, FILTER_TFT){
 	bool res = StatusMock::filter("abcdEFGH", "[a-z]{8}", true, false, true);
 	EXPECT_TRUE(res);
 
@@ -49,7 +66,7 @@ TEST(Status, FILTER_TFT){
 
 }
 
-TEST(Status, FILTER_FTT){
+TEST_F(StatusTest, FILTER_FTT){
 	bool res = StatusMock::filter("abcdEFGH", "abcdEFGH", false, true, true);
 	EXPECT_TRUE(res);
 
@@ -57,7 +74,7 @@ TEST(Status, FILTER_FTT){
 	EXPECT_FALSE(res);
 }
 
-TEST(Status, FILTER_TTT){
+TEST_F(StatusTest, FILTER_TTT){
 	bool res = StatusMock::filter("abcdEFGH", "\\w{8}", true, true, true);
 	EXPECT_TRUE(res);
 
@@ -65,10 +82,60 @@ TEST(Status, FILTER_TTT){
 	EXPECT_FALSE(res);
 }
 
-TEST(Status, FILTER_REGEX_TOLOWER){
+TEST_F(StatusTest, FILTER_REGEX_TOLOWER){
 	bool res = StatusMock::filter("abcdefgh ", "\\S{8}", true, true, false);
 	EXPECT_TRUE(res);
 
 	res = StatusMock::filter("abcdefgh ", "\\S{8}", true, false, false);		// regex toLower() cause problem
 	EXPECT_TRUE(res);
+}
+
+TEST_F(StatusTest, VIEW_NOT_NULL){
+	// We know `nullptr` evaluates to `0` which is `FALSE`
+	// This check `sm->get_view` does not return a null pointer
+	EXPECT_TRUE(sm.get_view()) << "get_view() unexpectedly returned a nullptr.";
+}
+
+// Recursive method to click all the checkboxes that are descendents of a Container Widget
+void StatusTest::click_everything(Gtk::Widget *obj){
+	// Attempt to cast the object as a Gtk::Button, then click it
+	// This will also cast CheckButtons which are a type of Button
+	Gtk::Button *bu  = dynamic_cast<Gtk::Button*>(obj);
+	// If cb is not a nullptr, then it is a Gtk::Button
+	if(bu){
+		// click the button
+		bu->clicked();
+	}
+
+	// Attempt to cast the object as a Gtk::Container, then enumerate children
+	Gtk::Container *parent = dynamic_cast<Gtk::Container*>(obj);
+	// If parent is not a nullptr, then it is a Gtk::Container
+	if(parent){
+		auto children = parent->get_children();
+		for(auto child : children){
+			click_everything(child);
+		}
+	}
+}
+
+TEST_F(StatusTest, REFRESH_SIGNAL_HANDLER){
+	// We know there are exactly three CheckButtons and one SearchEntry that can be modified to send a signal
+	// However, I was getting some problems emitting the `signal_search_changed` event, so this does not test the SearchEntry
+	// If there are a different amount of CheckButtons, this will fail.
+	EXPECT_CALL(*this, handle_signal()).Times(3);
+
+	auto fun = sigc::mem_fun(*this, &StatusTest::handle_signal);
+	sm.set_refresh_signal_handler(fun);
+	
+	click_everything(&sm);
+}
+
+TEST_F(StatusTest, APPLY_SIGNAL_HANDLER){
+	// We expect that there is exactly one button that can be used to 'apply' changes to a profile.
+	EXPECT_CALL(*this, handle_signal()).Times(1);
+
+	auto fun = sigc::mem_fun(*this, &StatusTest::handle_signal);
+	sm.set_apply_signal_handler(fun);
+
+	click_everything(&sm);
 }
--- test/src/tabs/status_mock.cc
+++ test/src/tabs/status_mock.cc
@@ -1,16 +1,20 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "../../../src/tabs/status.h"
 
 class StatusMock : public Status
 {
-	FRIEND_TEST(Status, FILTER_FFF);
-	FRIEND_TEST(Status, FILTER_TFF);
-	FRIEND_TEST(Status, FILTER_FTF);
-	FRIEND_TEST(Status, FILTER_FFT);
-	FRIEND_TEST(Status, FILTER_TTF);
-	FRIEND_TEST(Status, FILTER_TFT);
-	FRIEND_TEST(Status, FILTER_FTT);
-	FRIEND_TEST(Status, FILTER_TTT);
-	FRIEND_TEST(Status, FILTER_REGEX_TOLOWER);
+	FRIEND_TEST(StatusTest, FILTER_FFF);
+	FRIEND_TEST(StatusTest, FILTER_TFF);
+	FRIEND_TEST(StatusTest, FILTER_FTF);
+	FRIEND_TEST(StatusTest, FILTER_FFT);
+	FRIEND_TEST(StatusTest, FILTER_TTF);
+	FRIEND_TEST(StatusTest, FILTER_TFT);
+	FRIEND_TEST(StatusTest, FILTER_FTT);
+	FRIEND_TEST(StatusTest, FILTER_TTT);
+	FRIEND_TEST(StatusTest, FILTER_REGEX_TOLOWER);
+	FRIEND_TEST(StatusTest, VIEW_NOT_NULL);
+	FRIEND_TEST(StatusTest, APPLY_SIGNAL_HANDLER);
+	FRIEND_TEST(StatusTest, REFRESH_SIGNAL_HANDLER);
 };
