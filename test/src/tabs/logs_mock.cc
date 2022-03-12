c3c38901c9b13a3084d853967053ff53c3c337ba
Shay Rounsville <shr80@pitt.edu>
2022-03-12 18:04:22 -0500

    Changed method for getting logs
    Progress towards implementation of new log fetching method, using journalctl instead of dmesg

--- test/src/tabs/logs_mock.cc
+++ test/src/tabs/logs_mock.cc
@@ -6,10 +6,10 @@
 template<class ColumnRecord> class LogsMock : public Logs<ColumnRecord>
 {
 public:
-  FRIEND_TEST(LogsTabTest, TEST_ADD_ROW_FROM_LINE);
-  FRIEND_TEST(LogsTabTest, TEST_ADD_DATA_TO_RECORD);
-  FRIEND_TEST(LogsTabTest, TEST_PARSE_LINE);
-  FRIEND_TEST(LogsTabTest, TEST_REFRESH);
+  //FRIEND_TEST(LogsTabTest, TEST_ADD_ROW_FROM_LINE);
+  //FRIEND_TEST(LogsTabTest, TEST_ADD_DATA_TO_RECORD);
+  //FRIEND_TEST(LogsTabTest, TEST_PARSE_LINE);
+  //FRIEND_TEST(LogsTabTest, TEST_REFRESH);
 
   explicit LogsMock(std::shared_ptr<ColumnRecord> col_record) : Logs<ColumnRecord>(col_record) { }
 };
