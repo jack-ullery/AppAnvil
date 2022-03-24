0b3dd5fa55ea42dc83b28860284ed3bbc850fe7b
Maxwell Benko <47008373+mfb35@users.noreply.github.com>
2022-03-23 22:22:01 -0400

    Add files via upload

---
+++ CMakeFiles/FORMAT.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FORMAT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/FORMAT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/appanvil.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/appanvil.dir/src/main.cc.o"
+  "dist/appanvil"
+  "dist/appanvil.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/appanvil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
