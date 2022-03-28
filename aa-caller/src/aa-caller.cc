82c43bac3e40983fcc03eb15f509a3df54bd2e82
Shay Rounsville <shr80@pitt.edu>
2022-03-28 14:53:47 -0400

    Update aa-caller to work with new Logs

--- aa-caller/src/aa-caller.cc
+++ aa-caller/src/aa-caller.cc
@@ -42,7 +42,7 @@ std::string AppArmorCaller::get_unconfined(AppArmorCaller *caller)
 
 std::string AppArmorCaller::get_logs(AppArmorCaller *caller)
 {
-  std::vector<std::string> command = {"dmesg", "--ctime"};
+  std::vector<std::string> command = {"journalctl", "-r", "_AUDIT_TYPE=1400", "--output=json"};
   std::string return_on_error = "";
   return caller->call_command(command, return_on_error);
 }
