efcc8179f85c6d86f2799288fb690f1924f723b7
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2023-01-04 16:59:45 -0500

    create basic layout for 'ProfileModify'

--- src/tabs/view/profile_modify.cc
+++ src/tabs/view/profile_modify.cc
@@ -1,43 +1,13 @@
+#include "common.h"
 #include "profile_modify.h"
 #include "../../threads/command_caller.h"
 
 #include <apparmor_parser.hh>
-#include <fstream>
-#include <gtkmm/label.h>
-#include <memory>
-#include <pangomm/attributes.h>
-#include <pangomm/attrlist.h>
-#include <pangomm/fontdescription.h>
-#include <pangomm/layout.h>
 
 ProfileModify::ProfileModify(const std::string &profile_name)
-  : m_box{ new Gtk::VBox() }
+  : builder{ Gtk::Builder::create_from_resource("/resources/profile_modify.glade") },
+    m_box{ Common::get_widget<Gtk::Box>("m_box", builder) }
 {
-  //// Set values for the "header" widget ////
-  header.set_label("Modify Profile");
-
-  // Set the attributes for the "header" wiget
-  Pango::AttrList attr;
-
-  // Scale the header by 110%
-  constexpr double HEADER_SCALE = 1.1;
-  auto scale = Pango::Attribute::create_attr_scale(HEADER_SCALE);
-  attr.insert(scale);
-
-  // Make the header bold
-  auto weight = Pango::Attribute::create_attr_weight(Pango::WEIGHT_BOLD);
-  attr.insert(weight);
-
-  header.set_attributes(attr);
-
-  // Add the header to the main box
-  m_box->add(header);
-
-  // Add the main box to this widget
-  m_box->set_halign(Gtk::ALIGN_START);
-  m_box->set_valign(Gtk::ALIGN_START);
-
   this->add(*m_box);
-
   this->show_all();
 }
--- src/tabs/view/profile_modify.h
+++ src/tabs/view/profile_modify.h
@@ -2,6 +2,7 @@
 #define TABS_PROFILE_MODIFY_H
 
 #include <gtkmm/box.h>
+#include <gtkmm/builder.h>
 #include <gtkmm/label.h>
 #include <gtkmm/scrolledwindow.h>
 #include <list>
@@ -15,11 +16,11 @@ public:
   explicit ProfileModify(const std::string &profile_name);
 
 private:
+  // GUI Builder to parse UI from xml file
+  Glib::RefPtr<Gtk::Builder> builder;
+
   // VBox which holds all the widgets
   std::unique_ptr<Gtk::Box> m_box;
-
-  // Widgets
-  Gtk::Label header;
 };
 
 #endif
