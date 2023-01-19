3c5b4a8c00bd0f90eacefb628ccc4fd05dd6ce4e
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2023-01-19 18:16:13 -0500

    read file rules of profile

--- src/tabs/view/profile_modify.cc
+++ src/tabs/view/profile_modify.cc
@@ -3,42 +3,75 @@
 #include "edit_row.h"
 #include "profile_modify.h"
 
+#include <apparmor_file_rule.hh>
 #include <apparmor_parser.hh>
 #include <apparmor_profile.hh>
 #include <gtkmm/enums.h>
 #include <gtkmm/label.h>
+#include <memory>
 #include <string>
 #include <unordered_set>
 
 // Given a parsed file, return a particular profile corresponding to the "profile_name"
-std::unordered_set<std::string> ProfileModify::get_abstractions(const AppArmor::Parser &parsed_profile, const std::string &profile_name)
+std::unique_ptr<AppArmor::Profile> ProfileModify::find_profile(const AppArmor::Parser &parsed_profile, const std::string &profile_name)
 {
   auto profile_list = parsed_profile.getProfileList();
   
   for(auto &profile : profile_list) {
     if(profile.getName() == profile_name)
     {
-      return profile.getAbstractions();
+      return std::make_unique<AppArmor::Profile>(profile);
     }
   }
 
-  return std::unordered_set<std::string>();
+  return std::unique_ptr<AppArmor::Profile>(nullptr);
 }
 
-void ProfileModify::intialize_abstractions(const AppArmor::Parser &parsed_profile, const std::string &profile_name)
+void ProfileModify::intialize_abstractions(const std::unique_ptr<AppArmor::Profile> &profile)
 {
-  auto abstractions = get_abstractions(parsed_profile, profile_name);
+  if(profile != nullptr)
+  {
+    auto abstractions = profile->getAbstractions();
 
-  for(const std::string &abstraction : abstractions) {
-    auto label = std::make_shared<EditRow>(abstraction);
-    label->set_halign(Gtk::ALIGN_START);
+    for(const std::string &abstraction : abstractions) {
+      auto label = std::make_shared<EditRow>(abstraction);
+      label->set_halign(Gtk::ALIGN_START);
 
-    m_abstraction_box->pack_end(*label);
+      m_abstraction_box->pack_end(*label);
 
-    // Insert the widget into the map
-    // This ensures it is not deleted before it is drawn
-    // Also we will want to retrienve the widget later and delete it, if the abstraction is removed
-    abstraction_map.emplace(abstraction, label);
+      // Insert the widget into the map
+      // This ensures it is not deleted before it is drawn
+      // Also we will want to retrieve the widget later and delete it, if the abstraction is removed
+      abstraction_map.emplace(abstraction, label);
+    }
+  }
+}
+
+#include <iostream>
+
+void ProfileModify::intialize_file_rules(const std::unique_ptr<AppArmor::Profile> &profile)
+{
+  if(profile != nullptr)
+  {
+    auto rules = profile->getFileRules();
+
+    std::cout << "rules: " << rules.size() << std::endl;
+
+    for(AppArmor::FileRule &rule : rules) {
+      std::string title = rule.getFilename();
+      auto label = std::make_shared<EditRow>(title);
+      label->set_halign(Gtk::ALIGN_START);
+
+      m_file_rule_box->pack_end(*label);
+
+      // Insert the widget into the map
+      // This ensures it is not deleted before it is drawn
+      // Also we will want to retrieve the widget later and delete it, if the abstraction is removed
+      auto rule_ptr = std::make_shared<AppArmor::FileRule>(rule);
+      file_rule_map.emplace(rule_ptr, label);
+
+      std::cout << title << std::endl;
+    }
   }
 }
 
@@ -46,21 +79,28 @@ ProfileModify::ProfileModify(const std::string &profile_name)
   : builder{ Gtk::Builder::create_from_resource("/resources/profile_modify.glade") },
     m_box{ Common::get_widget<Gtk::Box>("m_box", builder) },
     m_title{ Common::get_widget<Gtk::Label>("m_title", builder) },
-    m_abstraction_box{ Common::get_widget<Gtk::Box>("m_abstraction_box", builder) }
+    m_abstraction_box{ Common::get_widget<Gtk::Box>("m_abstraction_box", builder) },
+    m_file_rule_box{ Common::get_widget<Gtk::Box>("m_file_rule_box", builder) }
 {
   m_title->set_label(profile_name);
 
   // Attempt to find and parse this profile
   std::string profile_location = CommandCaller::locate_profile(profile_name) + profile_name;
   std::fstream profile_stream(profile_location);
-  AppArmor::Parser parsed_profile(profile_stream);
+  AppArmor::Parser parsed(profile_stream);
 
   // Left justify the children of this box
   m_abstraction_box->set_halign(Gtk::ALIGN_START);
   m_abstraction_box->set_valign(Gtk::ALIGN_START);
 
-  // Initialize the list of abstractions using the parsed profile
-  intialize_abstractions(parsed_profile, profile_name);
+  // Find this profile from the file
+  std::unique_ptr<AppArmor::Profile> found_profile = find_profile(parsed, profile_name);
+
+  if(found_profile != nullptr) {
+    // Initialize the lists of abstractions and file_rules using the parsed profile
+    intialize_abstractions(found_profile);
+    intialize_file_rules(found_profile);
+  }
 
   this->add(*m_box);
   this->show_all();
--- src/tabs/view/profile_modify.h
+++ src/tabs/view/profile_modify.h
@@ -1,7 +1,9 @@
 #ifndef TABS_PROFILE_MODIFY_H
 #define TABS_PROFILE_MODIFY_H
 
+#include <apparmor_file_rule.hh>
 #include <apparmor_parser.hh>
+#include <apparmor_profile.hh>
 #include <gtkmm/box.h>
 #include <gtkmm/builder.h>
 #include <gtkmm/label.h>
@@ -17,11 +19,16 @@ public:
   explicit ProfileModify(const std::string &profile_name);
 
 protected:
-  // Non-static helper functions
-  void intialize_abstractions(const AppArmor::Parser &parsed_profile, const std::string &profile_name);
-
   // Static helper functions  
-  static std::unordered_set<std::string> get_abstractions(const AppArmor::Parser &parsed_profile, const std::string &profile_name);
+  /**
+   *  Attempt to find a the profile that corresponds to "profile_name" from the parsed profiles.
+   *  Returns a reference to a nullptr if no profile was found.
+   */
+  static std::unique_ptr<AppArmor::Profile> find_profile(const AppArmor::Parser &parsed_profile, const std::string &profile_name);
+
+  // Non-static helper functions
+  void intialize_abstractions(const std::unique_ptr<AppArmor::Profile> &profile);
+  void intialize_file_rules(const std::unique_ptr<AppArmor::Profile> &profile);
 
 private:
   // GUI Builder to parse UI from xml file
@@ -33,9 +40,13 @@ private:
   // Widgets
   std::unique_ptr<Gtk::Label> m_title;
   std::unique_ptr<Gtk::Box>   m_abstraction_box;
+  std::unique_ptr<Gtk::Box>   m_file_rule_box;
 
   // Container of added abstractions
   std::map<std::string, std::shared_ptr<Gtk::Widget>> abstraction_map;
+
+  // Container of added file rules
+  std::map<std::shared_ptr<AppArmor::FileRule>, std::shared_ptr<Gtk::Widget>> file_rule_map;
 };
 
 #endif
