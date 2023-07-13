c9e1c86f2a33f95e2cfdac3a3dc0db6e0276436a
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2023-07-12 20:04:18 -0400

    add ability to read audit logs using ausearch

--- aa-caller/src/aa-caller.cc
+++ aa-caller/src/aa-caller.cc
@@ -1,12 +1,21 @@
 #include "aa-caller.h"
 
+#include <cstdlib>
 #include <glibmm/spawn.h>
 #include <iostream>
 
 AppArmorCaller::results AppArmorCaller::call_command(const std::vector<std::string> &command)
 {
   results result;
-  Glib::spawn_sync("/usr/sbin/", command, Glib::SpawnFlags::SPAWN_SEARCH_PATH, {}, &result.output, &result.error, &result.exit_status);
+  std::vector<std::string> envp = {"/usr/bin/", "/usr/sbin/"};
+  Glib::spawn_sync("/usr/sbin/",
+                   command,
+                   envp,
+                   Glib::SpawnFlags::SPAWN_SEARCH_PATH_FROM_ENVP,
+                   {},
+                   &result.output,
+                   &result.error,
+                   &result.exit_status);
   return result;
 }
 
@@ -18,9 +27,14 @@ std::string AppArmorCaller::call_command(const std::vector<std::string> &command
 
   results result = call_command(command);
 
+  // If there was an error, print it out to std::cerr
   if (result.exit_status != 0) {
-    std::cout << "Error calling '" << command[0] << "'. " << result.error << std::endl;
-    return return_on_error;
+    // ausearch returns and error code if no data was found
+    // I do not consider that an issue, so I suppress it here
+    if(command[0] != "ausearch" || result.exit_status != 256) {
+      std::cerr << "Error calling '" << command[0] << "' returned (" << result.exit_status << "). " << result.error << std::endl;
+      return return_on_error;
+    }
   }
 
   return result.output;
@@ -40,6 +54,14 @@ std::string AppArmorCaller::get_unconfined(AppArmorCaller *caller)
   return caller->call_command(command, return_on_error);
 }
 
+std::string AppArmorCaller::get_logs(AppArmorCaller *caller, const std::string &filename)
+{
+  std::vector<std::string> command = {"ausearch", "--raw", "-m", "AVC", "--checkpoint", filename};
+
+  std::string return_on_error;
+  return caller->call_command(command, return_on_error);
+}
+
 // Static public methods
 std::string AppArmorCaller::get_status()
 {
@@ -51,4 +73,25 @@ std::string AppArmorCaller::get_unconfined()
 {
   AppArmorCaller caller;
   return get_unconfined(&caller);
+}
+
+std::string AppArmorCaller::get_logs()
+{
+  AppArmorCaller caller;
+
+  // Create a random filename with the following format '/tmp/appanvil-$RAND' 
+  std::stringstream stream;
+  stream << "/tmp/appanvil-" << rand();
+
+  // Call get_logs and append the output to what we are interested in
+  std::string output = get_logs(&caller, stream.str());
+  stream << std::endl << output;
+
+  return stream.str();
+}
+
+std::string AppArmorCaller::get_logs(const std::string &filename)
+{
+  AppArmorCaller caller;
+  return get_logs(&caller, filename);
 }
--- aa-caller/src/main.cc
+++ aa-caller/src/main.cc
@@ -4,11 +4,14 @@
 
 void print_usage()
 {
-  std::cout << "A utility to do common AppArmor tasks (used by the AppAnvil Project)" << std::endl << std::endl;
+  std::cout << "A utility to do common AppArmor tasks (used internally by the AppAnvil Project)" << std::endl;
+  std::cout << "This tool is not intended for direct use by the end-user" << std::endl << std::endl;
   std::cout << "Usage: aa-caller <option>" << std::endl << std::endl;
   std::cout << "Options:" << std::endl;
   std::cout << "   -s :  call \"aa-status\", to get the status of currently confined apps" << std::endl;
   std::cout << "   -u :  call \"aa-unconfined\", to get the status of some unconfined apps" << std::endl;
+  std::cout << "   -l :  call \"ausearch\", to get auditd logs. First line of output is a randomly-named checkpoint file." << std::endl;
+  std::cout << "   -l [filename]:  call \"ausearch\", to get auditd logs, using a previously generated checkpoint file." << std::endl;
 }
 
 int main(int argc, char **argv)
@@ -25,6 +28,21 @@ int main(int argc, char **argv)
     if (arg == "-u") {
       // Argument for: "get_unconfined"
       std::cout << AppArmorCaller::get_unconfined();
+      return 0;
+    }
+
+    if (arg == "-l") {
+      // Argument for: "get_logs"
+      std::cout << AppArmorCaller::get_logs();
+      return 0;
+    }
+  }
+  else if (argc == 3) {
+    std::string arg_1(argv[1]);
+    std::string arg_2(argv[2]);
+    if (arg_1 == "-l") {
+      // Argument for: "get_logs"
+      std::cout << AppArmorCaller::get_logs(arg_2);
       return 0;
     }
   }
