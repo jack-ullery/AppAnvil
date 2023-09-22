56f69cf4d488c83b31e72f07bdfd8a17010e0c5f
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2023-09-22 18:46:17 -0400

    add mocked function

--- test/src/libs/apparmor_parser_mock.h
+++ test/src/libs/apparmor_parser_mock.h
@@ -39,6 +39,7 @@ public:
   MOCK_METHOD(bool, hasChanges, ());
   MOCK_METHOD(void, saveChanges, ());
   MOCK_METHOD(void, cancelChanges, ());
+  MOCK_METHOD(std::string, getPath, ());
 
   operator std::string() {
     return "this is a string";
