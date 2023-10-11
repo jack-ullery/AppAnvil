be1edada6c3311f67d1d688d7a03bed4b25cf858
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2023-10-11 15:38:50 -0400

    restore deleted controller

---
+++ src/tabs/controller/profile_loader_controller.cc
@@ -0,0 +1,45 @@
+#include "profile_loader_controller.h"
+#include "../../threads/command_caller.h"
+#include "../view/profile_loader.h"
+
+// TODO(apparmor): Need to make asynchronous eventually, there should be no direct calls to CommandCaller
+template<class ProfileLoaderTab>
+void ProfileLoaderController<ProfileLoaderTab>::on_confirm_clicked()
+{
+  // In production-code, this is a `Glib::RefPtr<Gio::File>`
+  auto file = fc->get_file();
+  
+  // If the file is not null, then attempt to load it as a new profiile
+  if (file) {
+  	auto filename = file->get_path(); 
+	  std::string short_filename = CommandCaller::load_profile(filename);
+
+    // TODO(apparmor): Add error handling, when the profile is not correctly formatted
+	  fc->set_label_text("Done");
+	}
+  else {
+	  fc->set_label_text("No profile selected!");
+  }
+}
+
+template<class ProfileLoaderTab>
+void ProfileLoaderController<ProfileLoaderTab>::clearLabel(){
+  fc->set_label_text("");
+}
+
+template<class ProfileLoaderTab>
+std::shared_ptr<ProfileLoaderTab> ProfileLoaderController<ProfileLoaderTab>::get_tab()
+{
+  return fc;
+}
+
+template<class ProfileLoaderTab>
+ProfileLoaderController<ProfileLoaderTab>::ProfileLoaderController()
+  : fc{new ProfileLoaderTab()}
+{
+  auto button_func = sigc::mem_fun(*this, &ProfileLoaderController::on_confirm_clicked);
+  fc->set_l_button_signal_handler(button_func);
+}
+
+// Used to avoid linker errors
+template class ProfileLoaderController<ProfileLoader>;
