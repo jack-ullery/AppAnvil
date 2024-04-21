11a6c1f134c26eefe3092a126f4b5a2aacb1639e
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2024-04-21 15:41:24 -0400

    implment logic for adding new FileRule

--- src/tabs/view/add_file_rule.cc
+++ src/tabs/view/add_file_rule.cc
@@ -1,9 +1,12 @@
 #include <fstream>
 #include <gtkmm/dialog.h>
+#include <gtkmm/filechooserdialog.h>
 #include <gtkmm/liststore.h>
 #include <gtkmm/treemodelcolumn.h>
 #include <gtkmm/treemodelsort.h>
 #include <libappanvil/tree/FileRule.hh>
+#include <tree/FileMode.hh>
+#include <tree/PrefixNode.hh>
 #include <utility>
 
 #include "add_file_rule.h"
@@ -21,8 +24,16 @@ AddFileRule::AddFileRule()
     button_back{ Common::get_widget<Gtk::Button>("button_back", builder)},
     button_accept{ Common::get_widget<Gtk::Button>("button_accept", builder)},
     fr_entry{ Common::get_widget<Gtk::Entry>("fr_entry", builder) },
+    fr_entry_button{ Common::get_widget<Gtk::Button>("fr_entry_button", builder) },
     fr_preview{ Common::get_widget<Gtk::TextView>("fr_preview", builder) },
-    fr_text{ Common::get_widget<Gtk::TextView>("fr_text", builder) }
+    cb_read{ Common::get_widget<Gtk::CheckButton>("cb_read", builder) },
+    cb_write{ Common::get_widget<Gtk::CheckButton>("cb_write", builder) },
+    cb_exec{ Common::get_widget<Gtk::CheckButton>("cb_exec", builder) },
+    cb_link{ Common::get_widget<Gtk::CheckButton>("cb_link", builder) },
+    cb_lock{ Common::get_widget<Gtk::CheckButton>("cb_lock", builder) }, 
+    cb_deny{ Common::get_widget<Gtk::CheckButton>("cb_deny", builder) },
+    cb_owner{ Common::get_widget<Gtk::CheckButton>("cb_owner", builder) },
+    cb_audit{ Common::get_widget<Gtk::CheckButton>("cb_audit", builder) }
 {
     // Connect the buttons
     auto handle_button_cancel = sigc::mem_fun(*this, &AddFileRule::handle_button_cancel);
@@ -34,9 +45,21 @@ AddFileRule::AddFileRule()
     button_back->signal_clicked().connect(handle_button_back);
     button_accept->signal_clicked().connect(handle_button_accept);
 
-    // Connect 'fr_entry' widget
+    // Connect the file button
+    auto handle_file_button = sigc::mem_fun(*this, &AddFileRule::handle_file_button);
+    fr_entry_button->signal_clicked().connect(handle_file_button);
+
+    // If the entry or any of the checkboxes change, call `handle_entry_chancged()`
     auto handle_entry = sigc::mem_fun(*this, &AddFileRule::handle_entry_changed);
     fr_entry->signal_changed().connect(handle_entry);
+    cb_read->signal_clicked().connect(handle_entry);
+    cb_write->signal_clicked().connect(handle_entry);
+    cb_exec->signal_clicked().connect(handle_entry);
+    cb_link->signal_clicked().connect(handle_entry);
+    cb_lock->signal_clicked().connect(handle_entry);
+    cb_deny->signal_clicked().connect(handle_entry);
+    cb_owner->signal_clicked().connect(handle_entry);
+    cb_audit->signal_clicked().connect(handle_entry);
 
     handle_entry_changed();
 
@@ -66,28 +89,51 @@ void AddFileRule::handle_button_accept()
     dialog->response(Gtk::RESPONSE_ACCEPT);
 }
 
+void AddFileRule::handle_file_button()
+{
+    Gtk::FileChooserDialog dialog("~/");
+    dialog.set_create_folders(false);
+
+    auto status = dialog.run();
+    if(status == 0)
+    {
+        auto file = dialog.get_file();
+        auto filepath = file->get_uri();
+        fr_entry->set_text(filepath);
+    }
+}
+
 void AddFileRule::handle_entry_changed()
 {
-    // auto text = fr_entry->get_text();
-    // prospective_rule = AppArmor::Tree::FileRule(text);
+    bool audit = cb_audit->get_active();
+    bool deny  = cb_deny->get_active();
+    bool owner = cb_owner->get_active();
+    AppArmor::Tree::PrefixNode prefix(audit, deny, owner);
 
-    // if(text.empty())
-    // {
-    //     button_next->set_sensitive(false);
-    // } else {
-    //     button_next->set_sensitive(true);
+    bool read  = cb_read->get_active();
+    bool write = cb_write->get_active();
+    bool append = false;
+    bool memory_map = false;
+    bool exec  = cb_exec->get_active();
+    bool link  = cb_link->get_active();
+    bool lock  = cb_lock->get_active();
+    std::string exec_mode = exec? (deny? "x" : "cix") : "";
+    AppArmor::Tree::FileMode mode(read, write, append, memory_map, link, lock, exec_mode);
 
-    //     // Populate the "FileRule Preview" TextView
-    //     auto preview_text = prospective_rule.operator std::string();
-    //     fr_preview->get_buffer()->set_text(preview_text);
+    auto text = fr_entry->get_text();
+    prospective_rule = AppArmor::Tree::FileRule(text, mode);
+    prospective_rule.setPrefix(prefix);
 
-    //     // Populate the "FileRule Text" TextView
-    //     auto file_rule_path = "/etc/apparmor.d/file_rules/" + text;
-    //     std::ifstream file_rule_file(file_rule_path);
-    //     std::stringstream file_rule_stream;
-    //     file_rule_stream << file_rule_file.rdbuf();
-    //     fr_text->get_buffer()->set_text(file_rule_stream.str());
-    // }
+    if(text.empty())
+    {
+        button_next->set_sensitive(false);
+    } else {
+        button_next->set_sensitive(true);
+
+        // Populate the "FileRule Preview" TextView
+        auto preview_text = prospective_rule.operator std::string();
+        fr_preview->get_buffer()->set_text(preview_text);
+    }
 }
 
 AddFileRule::show_dialog_response AddFileRule::show_dialog()
--- src/tabs/view/add_file_rule.h
+++ src/tabs/view/add_file_rule.h
@@ -5,6 +5,7 @@
 #include <gtkmm/box.h>
 #include <gtkmm/builder.h>
 #include <gtkmm/button.h>
+#include <gtkmm/checkbutton.h>
 #include <gtkmm/combobox.h>
 #include <gtkmm/dialog.h>
 #include <gtkmm/entry.h>
@@ -30,6 +31,7 @@ protected:
     AddFileRule();
 
     // Signal Handlers
+    void handle_file_button();
     void handle_entry_changed();
     void handle_button_cancel();
     void handle_button_next();
@@ -52,8 +54,18 @@ private:
     std::unique_ptr<Gtk::Button> button_accept;
 
     std::unique_ptr<Gtk::Entry>    fr_entry;
+    std::unique_ptr<Gtk::Button>   fr_entry_button;
     std::unique_ptr<Gtk::TextView> fr_preview;
-    std::unique_ptr<Gtk::TextView> fr_text;
+
+    std::unique_ptr<Gtk::CheckButton> cb_read;
+    std::unique_ptr<Gtk::CheckButton> cb_write;
+    std::unique_ptr<Gtk::CheckButton> cb_exec;
+    std::unique_ptr<Gtk::CheckButton> cb_link;
+    std::unique_ptr<Gtk::CheckButton> cb_lock;
+ 
+    std::unique_ptr<Gtk::CheckButton> cb_deny;
+    std::unique_ptr<Gtk::CheckButton> cb_owner;
+    std::unique_ptr<Gtk::CheckButton> cb_audit;
 
     // Currently selected rule (if there is one)
     AppArmor::Tree::FileRule prospective_rule;
